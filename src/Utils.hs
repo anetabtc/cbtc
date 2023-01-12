890f72140e34b6f3d7ef918690a07383c3841bac
SolidSnake <solid@snake>
2023-01-12 16:33:46 -0500

    add phasOneCurrecySymbolOneTokenName function

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,4 +1,7 @@
-module Utils (evalT) where
+module Utils (
+  evalT,
+  phasOneCurrecySymbolOneTokenName,
+) where
 
 import Data.Bifunctor (
   first,
@@ -8,17 +11,20 @@ import Data.Text (
   pack,
  )
 import Plutarch (
-  ClosedTerm,
   Config (Config),
   TracingMode (DoTracing),
   compile,
  )
+import Plutarch.Api.V2 (AmountGuarantees, KeyGuarantees, PMap (PMap), PValue (PValue))
 import Plutarch.Evaluate (
   evalScript,
  )
+import "liqwid-plutarch-extra" Plutarch.Extra.List (pisSingleton)
 import "liqwid-plutarch-extra" Plutarch.Extra.Script (
   applyArguments,
  )
+import "liqwid-plutarch-extra" Plutarch.Extra.TermCont (pmatchC)
+import Plutarch.Prelude
 import Plutarch.Script (
   Script,
  )
@@ -36,3 +42,21 @@ evalWithArgsT x args = do
   let (escr, budg, trc) = evalScript $ applyArguments cmp args
   scr <- first (pack . show) escr
   pure (scr, budg, trc)
+
+{- | Returns 'PTrue' if the argument 'PValue' has one 'PCurrencySymbol'
+  and one 'PTokenName', if PValue is not /normalized/ ('PValue' ''Sorted' ''NonZero') it will return 'PFalse'
+-}
+phasOneCurrecySymbolOneTokenName ::
+  forall (keys :: KeyGuarantees) (amounts :: AmountGuarantees) (s :: S).
+  Term s (PValue keys amounts :--> PBool)
+phasOneCurrecySymbolOneTokenName = plam $ \value' ->
+  unTermCont $ do
+    PValue mapValue <- pmatchC $ value'
+    PMap listCS <- pmatchC mapValue
+    PMap listTokenAndAmnt <- pmatchC $ pfromData $ psndBuiltin #$ phead # listCS
+    pure $
+      ( pisSingleton
+          # listCS
+          #&& pisSingleton
+          # listTokenAndAmnt
+      )
