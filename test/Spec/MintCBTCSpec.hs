ad443ddd521faf82b56badf1e22e1a7baac15cf9
SolidSnake <solid@snake>
2023-01-17 13:30:46 -0500

    improve ScriptContext with checkPhase1

--- test/Spec/MintCBTCSpec.hs
+++ test/Spec/MintCBTCSpec.hs
@@ -11,7 +11,24 @@ module Spec.MintCBTCSpec (
 
 import MintCBTC qualified
 import Plutarch (Term)
-import "plutarch-context-builder" Plutarch.Context (Builder, MintingBuilder, buildMinting', input, mint, pubKey, script, withMinting, withRefIndex, withRefTxId, withValue)
+import "plutarch-context-builder" Plutarch.Context (
+  Builder,
+  MintingBuilder,
+  buildMinting,
+  checkPhase1,
+  input,
+  mint,
+  output,
+  pubKey,
+  script,
+  signedWith,
+  txId,
+  withMinting,
+  withRedeemer,
+  withRefIndex,
+  withRefTxId,
+  withValue,
+ )
 import Plutarch.Lift (pconstant)
 import Plutarch.Prelude (POpaque, pconstantData, (#))
 import Plutarch.Test.Precompiled (
@@ -25,7 +42,6 @@ import PlutusLedgerApi.V2 (
   ScriptContext (..),
   ScriptHash (..),
   TokenName (..),
-  TxId,
   adaSymbol,
   adaToken,
   singleton,
@@ -38,11 +54,11 @@ import Test.Tasty (
 sampleScriptHash :: ScriptHash
 sampleScriptHash = "395e0b6c308dbdfd6e41354b68f833b96990ecd93721699ed90a2113"
 
-samplePubKeyHash :: PubKeyHash
-samplePubKeyHash = "0d342d962a7aaac57e30d3f8dd2f41907a361860f8889253ebe40bbb"
+samplePubKeyHash1 :: PubKeyHash
+samplePubKeyHash1 = "0d342d962a7aaac57e30d3f8dd2f41907a361860f8889253ebe40bbb"
 
-sampleTxId :: TxId
-sampleTxId = "376efc6f1e85da82e082eeeb795fc34733991c1c190707722f23e9bf"
+samplePubKeyHash2 :: PubKeyHash
+samplePubKeyHash2 = "ea2484f839e72f5bd60e004e74b564bb75f79a980b22c55d88f4b8bb"
 
 inputScript :: Builder a => a
 inputScript =
@@ -50,18 +66,19 @@ inputScript =
     mconcat
       [ script sampleScriptHash
       , withValue (singleton adaSymbol adaToken 1)
-      , withRefTxId sampleTxId
+      , withRefTxId "759d3795c282bc2680d5541faa409f789cde81df369d03d057e4b58954ed865b"
       , withRefIndex 1
+      , withRedeemer (PlutusTx.toData ())
       ]
 
 inputPubKey :: Builder a => a
 inputPubKey =
   input $
     mconcat
-      [ pubKey samplePubKeyHash
+      [ pubKey samplePubKeyHash1
       , withValue (singleton adaSymbol adaToken 1)
-      , withRefTxId sampleTxId
-      , withRefIndex 1
+      , withRefTxId "24625f40313747ed839c2e20de5c1e2040c01411e6f528ee4b4abae5115c6608"
+      , withRefIndex 2
       ]
 
 commonCS :: MintingBuilder
@@ -69,72 +86,167 @@ commonCS = withMinting $ CurrencySymbol "currency-symbol-one"
 
 goodCtx1 :: ScriptContext
 goodCtx1 =
-  buildMinting' $
+  buildMinting checkPhase1 $
     mconcat
       [ mint $ singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 1
       , inputScript
       , inputPubKey
+      , outputPubKey
+      , signedWith samplePubKeyHash1
+      , txId "b2dfbe34017b9061464f401ec924ece385bb3ec07061c27907844b4d3ef6666e"
       , commonCS
       ]
+  where
+    outputPubKey :: Builder a => a
+    outputPubKey =
+      output $
+        mconcat
+          [ pubKey samplePubKeyHash2
+          , withValue
+              ( singleton adaSymbol adaToken 2
+                  <> singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 1
+              )
+          ]
 
 goodCtx2 :: ScriptContext
 goodCtx2 =
-  buildMinting' $
+  buildMinting checkPhase1 $
     mconcat
       [ mint $ singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 10
       , inputScript
       , inputPubKey
+      , outputPubKey
+      , signedWith samplePubKeyHash1
+      , txId "b2dfbe34017b9061464f401ec924ece385bb3ec07061c27907844b4d3ef6666e"
       , commonCS
       ]
+  where
+    outputPubKey :: Builder a => a
+    outputPubKey =
+      output $
+        mconcat
+          [ pubKey samplePubKeyHash2
+          , withValue
+              ( singleton adaSymbol adaToken 2
+                  <> singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 10
+              )
+          ]
 
 badCtx1 :: ScriptContext
 badCtx1 =
-  buildMinting' $
+  buildMinting checkPhase1 $
     mconcat
-      [ mint $
-          singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 2
-            <> singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-two") 2
+      [ mint $ oneCurrencySymboldualTokenName
       , inputScript
       , inputPubKey
+      , outputPubKey
+      , signedWith samplePubKeyHash1
+      , txId "b2dfbe34017b9061464f401ec924ece385bb3ec07061c27907844b4d3ef6666e"
       , commonCS
       ]
+  where
+    oneCurrencySymboldualTokenName =
+      singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 2
+        <> singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-two") 2
+
+    outputPubKey :: Builder a => a
+    outputPubKey =
+      output $
+        mconcat
+          [ pubKey samplePubKeyHash2
+          , withValue
+              ( singleton adaSymbol adaToken 2
+                  <> singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 2
+                  <> singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-two") 2
+              )
+          ]
 
 badCtx2 :: ScriptContext
 badCtx2 =
-  buildMinting' $
+  buildMinting checkPhase1 $
     mconcat
-      [ mint $
-          singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 2
-            <> singleton (CurrencySymbol "currency-symbol-two") (TokenName "token-two") 2
+      [ mint $ dualCurrencySymboldualTokenName
       , inputScript
       , inputPubKey
+      , outputPubKey
+      , signedWith samplePubKeyHash1
+      , txId "b2dfbe34017b9061464f401ec924ece385bb3ec07061c27907844b4d3ef6666e"
       , commonCS
       ]
+  where
+    dualCurrencySymboldualTokenName =
+      singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 2
+        <> singleton (CurrencySymbol "currency-symbol-two") (TokenName "token-two") 2
 
+    outputPubKey :: Builder a => a
+    outputPubKey =
+      output $
+        mconcat
+          [ pubKey samplePubKeyHash2
+          , withValue
+              ( singleton adaSymbol adaToken 2
+                  <> singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 2
+                  <> singleton (CurrencySymbol "currency-symbol-two") (TokenName "token-two") 2
+              )
+          ]
+
+-- | Wrong ScriptHash
 badCtx3 :: ScriptContext
 badCtx3 =
-  buildMinting' $
+  buildMinting checkPhase1 $
     mconcat
       [ mint $ singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 1
-      , input $
-          mconcat
-            [ script "8005f297da8c474354b9fecb481220ea7c76aea178ff7906bfdfb74f"
-            , withValue (singleton adaSymbol adaToken 1)
-            , withRefTxId sampleTxId
-            , withRefIndex 1
-            ]
+      , wrongScriptInput
       , inputPubKey
+      , outputPubKey
+      , signedWith samplePubKeyHash1
+      , txId "b2dfbe34017b9061464f401ec924ece385bb3ec07061c27907844b4d3ef6666e"
       , commonCS
       ]
+  where
+    wrongScriptInput =
+      input $
+        mconcat
+          [ script "8005f297da8c474354b9fecb481220ea7c76aea178ff7906bfdfb74f"
+          , withValue (singleton adaSymbol adaToken 1)
+          , withRefTxId "24625f40313747ed839c2e20de5c1e2040c01411e6f528ee4b4abae5115c6608"
+          , withRefIndex 1
+          , withRedeemer (PlutusTx.toData ())
+          ]
+    outputPubKey :: Builder a => a
+    outputPubKey =
+      output $
+        mconcat
+          [ pubKey samplePubKeyHash2
+          , withValue
+              ( singleton adaSymbol adaToken 2
+                  <> singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 1
+              )
+          ]
 
+-- | Missing Script Input
 badCtx4 :: ScriptContext
 badCtx4 =
-  buildMinting' $
+  buildMinting checkPhase1 $
     mconcat
       [ mint $ singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 1
       , inputPubKey
+      , outputPubKey
+      , signedWith samplePubKeyHash1
+      , txId "b2dfbe34017b9061464f401ec924ece385bb3ec07061c27907844b4d3ef6666e"
       , commonCS
       ]
+  where
+    outputPubKey :: Builder a => a
+    outputPubKey =
+      output $
+        mconcat
+          [ pubKey samplePubKeyHash2
+          , withValue
+              ( singleton adaSymbol adaToken 1
+                  <> singleton (CurrencySymbol "currency-symbol-one") (TokenName "token-one") 1
+              )
+          ]
 
 sampleTest :: TestTree
 sampleTest = tryFromPTerm "Test MintCBTC" MintCBTC.policy $ do
@@ -174,7 +286,7 @@ sampleTest = tryFromPTerm "Test MintCBTC" MintCBTC.policy $ do
     , PlutusTx.toData badCtx3
     ]
   testEvalCase
-    "Fail - Missing ScriptHash"
+    "Fail - Missing Script Input"
     Failure
     [ PlutusTx.toData sampleScriptHash
     , PlutusTx.toData ()
