f949163824ea8bc8918d2f47a6f375d0b66f7cd3
mnamadina <madina@brandeis.edu>
2023-04-06 23:51:49 -0400

    some chances to ada_addr

--- front-end/components/MainServer.tsx
+++ front-end/components/MainServer.tsx
@@ -421,7 +421,7 @@ function Run({ lucid }: Props){
 
 			// adding Madina's hash address
 			console.log("madina:")
-			let ada_addr = "addr_test1e17d9be587cafeb2aee690ecd71f8a77aa7c691a5a5903de677d693af76fabcf"
+			let ada_addr = "addr_e17d9be587cafeb2aee690ecd71f8a77aa7c691a5a5903de677d693af76fabcf"
 			let paymentCreds = lucid.utils.paymentCredentialOf(ada_addr)
 			console.log(paymentCreds.hash)
 			console.log(lucid.utils.credentialToAddress(paymentCreds))
