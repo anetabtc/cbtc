fecbd4d52934f9291edfbbf4bc746b66c1fba968
Boris P <boris@zfs.ninja>
2023-06-07 23:13:32 -0600

    add backend

---
+++ backend/next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  
+  output: 'standalone',
+  reactStrictMode: false,
+  webpack: (config) => {
+    config.experiments = { 
+      asyncWebAssembly: true,
+      topLevelAwait: true,
+      layers: true
+    }
+    return config
+  },
+  env:{
+    BLOCKFROST_KEY: process.env.BLOCKFROST_KEY,
+    API_URL: process.env.API_URL,
+    NETWORK: process.env.NETWORK
+  }
+}
+
+module.exports = nextConfig
---
+++ backend/utils/lucid.ts
@@ -0,0 +1,20 @@
+import { Constr, Data, Blockfrost, Lucid, Network } from 'lucid-cardano';
+
+const blockfrostKey = "preprodZ5sdrHtIciQKn3Ls3phRfCkqtVyFq5XX" //process.env.BLOCKFROST_KEY as string
+const apiURL = "https://cardano-preprod.blockfrost.io/api/v0" //process.env.API_URL as string
+const network = "Preprod" //process.env.NETWORK as Network
+
+// const initLucid = async (wallet: string) => {
+//     const api = await window.cardano[wallet.toLowerCase()].enable()
+//     const lucid = await Lucid.new(new Blockfrost(apiURL, blockfrostKey), network)
+//     lucid.selectWallet(api)
+//     return lucid;
+// }
+
+const initLucidWithoutWallet = async () => {
+    console.log(apiURL);
+    const lucid = await Lucid.new(new Blockfrost(apiURL, blockfrostKey), network)
+    return lucid;
+}
+
+export {initLucidWithoutWallet, Lucid, Data, Constr};
